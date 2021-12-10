a0c6a32d2c2b9d4d541a1af4780e60fac91a3c54
Eray Baran İnan <eraybaraninan@gmail.com>
2021-12-10 06:44:03 +0300

    Added: Road Creator

---
+++ Assets/Curve/Scripts/Editor/RoadEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Curve.Scripts.Examples;
+using UnityEngine;
+using UnityEditor;
+
+namespace Curve.Scripts.Editor
+{
+    [CustomEditor(typeof(RoadCreator))]
+    public class RoadEditor : UnityEditor.Editor
+    {
+        private RoadCreator _creator;
+
+        private void OnEnable()
+        {
+            _creator = (RoadCreator)target;
+        }
+
+        private void OnSceneGUI()
+        {
+            if (_creator.autoUpdate && Event.current.type == EventType.Repaint)
+            {
+                _creator.UpdateRoad();
+            }
+        }
+    }
+}
--- Assets/Curve/Scripts/Examples/RoadCreator.cs
+++ Assets/Curve/Scripts/Examples/RoadCreator.cs
@@ -2,8 +2,73 @@
 
 namespace Curve.Scripts.Examples
 {
+    [RequireComponent(typeof(PathCreator))]
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
     public class RoadCreator : MonoBehaviour
     {
+        [Range(.05f, 1.5f)]
+        public float spacing = 1f;
+        public float roadWidth = 1;
+
+        public bool autoUpdate;
+
+        public void UpdateRoad()
+        {
+            var path = GetComponent<PathCreator>().path;
+            var points = path.CalculateEvenlySpacedPoints(spacing);
+
+            var meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = CreateRoadMesh(points);
+        }
         
+        private Mesh CreateRoadMesh(Vector2[] points)
+        {
+            var verts = new Vector3[points.Length * 2];
+            var tris = new int[(points.Length - 1) * 2 * 3];
+            var vertexIndex = 0;
+            var triIndex = 0;
+
+            for (var i = 0; i < points.Length; i++)
+            {
+                var forward = Vector2.zero;
+
+                if (i < points.Length - 1)
+                {
+                    forward += points[i + 1] - points[i];
+                }
+
+                if (i > 0)
+                {
+                    forward += points[i] - points[i - 1];
+                }
+                forward.Normalize();
+
+                var left = new Vector2(-forward.y, forward.x);
+
+                verts[vertexIndex] = points[i] + left * roadWidth * .5f;
+                verts[vertexIndex + 1] = points[i] -left * roadWidth * .5f;
+
+                if (i < points.Length - 1)
+                {
+                    tris[triIndex] = vertexIndex;
+                    tris[triIndex + 1] = vertexIndex + 2;
+                    tris[triIndex + 2] = vertexIndex + 1;
+
+                    tris[triIndex + 3] = vertexIndex + 1;
+                    tris[triIndex + 4] = vertexIndex + 2;
+                    tris[triIndex + 5] = vertexIndex + 3;
+                }
+                
+                vertexIndex += 2;
+                triIndex += 6;
+            }
+
+            var mesh = new Mesh();
+            mesh.vertices = verts;
+            mesh.triangles = tris;
+
+            return mesh;
+        }
     }
 }
